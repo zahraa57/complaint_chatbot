a2cb6802122446dbd239769b38cae2f81ea7b9ca
Zahraa <Zahraa>
2023-12-15 07:04:06 +0200

    edited main function

--- chatbot_app/management/commands/chatbot_interaction.py
+++ chatbot_app/management/commands/chatbot_interaction.py
@@ -2,10 +2,8 @@
 import argparse
 import uuid
 from django.core.management.base import BaseCommand
-#from transformers import T5ForConditionalGeneration, T5Tokenizer
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-
-
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+#from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 from chatbot_app.models import Customer, ChatMessage, Complaint
 from django.core.exceptions import ValidationError
 
@@ -14,7 +12,7 @@ class Command(BaseCommand):
     help = 'Interact with customers using a chatbot'
 
     def handle(self, *args, **options):
-
+        # Get customer information
         while True:
             # Get customer information
             name = input("Enter customer name: ")
@@ -37,35 +35,51 @@ class Command(BaseCommand):
         customer.save()
 
         # Initialize the T5 transformer
-        model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
-        tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small")
+        model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-small")
+        tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small",legacy=True)
 
 
         #create the interactive chat
         conversation = ""
         while True:
+
             user_input=input("You: ")
+
             if user_input.lower() == 'exit':
                break
+               
+
             # Formulate the prompt with the summarization instruction
-            prompt = f"Answer the following question:\n{user_input}"
+            # Customize prompt based on user input
+            if "help" in user_input.lower():
+                prompt = f"You are the customer support assistant.\n{user_input}"
+            else:
+                prompt = f"Reasonaly answer the following question:\n{user_input}"
+
             inputs = tokenizer(prompt, return_tensors="pt")
-            outputs = model.generate(**inputs,max_length=500)
+
+            outputs = model.generate(**inputs,
+                                     max_length=500,
+                                     temperature=0.7,
+                                     num_beams=1,
+                                     do_sample=True)
+            
             chatbot_response=tokenizer.batch_decode(outputs, skip_special_tokens=True)
-            print(f"Chatbot: {chatbot_response}")
+            print(f"Chatbot: {chatbot_response[0]}")
 
             conversation += f"User: {user_input}\n"
-            conversation += f"chatbot: {chatbot_response}\n"
+            conversation += f"chatbot: {chatbot_response[0]}\n"
 
 
         # Log the chat message
         chat_message=ChatMessage.objects.create(customer=customer, message=conversation)
         chat_message.save()
-
+        
+        #Create the chat summary
         inputs = tokenizer("Summarize: "+conversation, return_tensors="pt")
         outputs = model.generate(**inputs,max_length=500)
         complaint_summary=tokenizer.batch_decode(outputs, skip_special_tokens=True)
-        print(f"summary: {complaint_summary}")
+        print(f"summary: {complaint_summary[0]}")
         # Store the summary and generate a unique ID for tracking
         unique_id = uuid.uuid4()
         complaint=Complaint.objects.create(customer=customer, summary=complaint_summary, unique_id=unique_id)
